46e01368519edb235e1b9ff286abeb7b7e480085
Aryan Deshmukh <aryan.deshmukh21@vit.edu>
2024-08-25 16:02:09 +0530

    memo sol 2

--- Week6/Hooks/src/components/Memo2.tsx
+++ Week6/Hooks/src/components/Memo2.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
 // You will learn something new here, specifically how you have to pass more than one value in the dependency array
 
-function Memo2() {
+const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
+const TOTAL_LINES = 1000;
+const ALL_WORDS : string[]= [];
+for (let i = 0; i < TOTAL_LINES; i++) {
+    let sentence = "";
+    for (let j = 0; j < words.length; j++) {
+        sentence += (words[Math.floor(words.length * Math.random())])
+        sentence += " "
+    }
+    ALL_WORDS.push(sentence);
+}
+
+
+const Memo2 :React.FC = () => {
+
+    const [input, setInput] = useState(0)
+    const [input2, setInput2] = useState(0)
+
+    const results = useMemo(() => {
+        return ALL_WORDS.slice(input, input2);
+    }, [input, input2])
+
   return (
-    <div>Memo2</div>
+    <div>
+        <input
+        value ={input}
+        onChange ={(e) => {setInput(Number(e.target.value))}}
+        >
+        </input>
+        <input
+        value ={input2}
+        onChange ={(e) => {setInput2(Number(e.target.value))}}
+        >
+        </input>
+        <div>
+            {results}
+        </div>
+    </div>
   )
 }
 
